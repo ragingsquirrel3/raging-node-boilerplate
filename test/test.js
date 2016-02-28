efe6f8e74ccabec196a6c96d62e237865b65baec
Travis Sheppard <travissheppard.1988@gmail.com>
2016-02-28 15:14:22 -0800

    babel test

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 // example basic test from mocha docs 
-const assert = require('assert');
+var assert = require('assert');
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
