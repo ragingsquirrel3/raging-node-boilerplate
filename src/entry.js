573d52eae23210d9f7a7a22d9bcdc4533c5ef25b
Travis Sheppard <travissheppard.1988@gmail.com>
2016-02-28 19:33:58 -0800

    start helix

--- src/entry.js
+++ src/entry.js
@@ -6,3 +6,9 @@
 
 // ReactDOM.render(React.createElement(ReduxApplication, {}), document.getElementById('target'));
 console.log('entry');
+
+// bp colors
+var A_COLOR = '#00A51D';
+var T_COLOR = '#F25270';
+var C_COLOR = '#F2E422';
+var G_COLOR = '#77468C';
