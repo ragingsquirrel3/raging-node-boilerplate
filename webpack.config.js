93469e97e1ebf6ac66c2f131949fa715d17c7b8c
Travis Sheppard <travissheppard.1988@gmail.com>
2016-02-28 16:59:07 -0800

    babel webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-    entry: './src/entry.js',
-    output: {
-        path: __dirname,
-        filename: 'build/dev/js/bundle.js'
-    }
+  entry: './src/entry.js',
+  output: {
+    path: __dirname,
+    filename: 'build/dev/js/bundle.js'
+  },
+  loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel', // 'babel-loader' is also a legal name to reference
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
 };
