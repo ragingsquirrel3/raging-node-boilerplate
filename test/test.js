67cc315b6f74f084f0ec222200b369cc388c515e
Travis <travissheppard.1988@gmail.com>
2016-03-08 21:11:15 -0800

    more basic test logic

--- test/test.js
+++ test/test.js
@@ -1,10 +1,7 @@
-// example basic test from mocha docs 
-var assert = require('assert');
-describe('Array', function () {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    });
+import assert from 'assert';
+import getOne from '../lib';
+describe('getOne()', function () {
+  it('should return 1', function () {
+    assert.equal(getOne(), 1);
   });
 });
