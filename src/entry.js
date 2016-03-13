4621bfdc33cd24e30238037fc7c268e465ee0f21
Travis <travissheppard.1988@gmail.com>
2016-03-12 23:11:13 -0800

    dna rendering

--- src/entry.js
+++ src/entry.js
@@ -4,9 +4,3 @@ import ReactDOM from 'react-dom';
 import DNA from './components/dna';
 
 ReactDOM.render(<DNA />, document.getElementById('target'));
-
-// bp colors
-var A_COLOR = '#00A51D';
-var T_COLOR = '#F25270';
-var C_COLOR = '#F2E422';
-var G_COLOR = '#77468C';
