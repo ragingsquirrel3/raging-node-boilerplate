de42006f4145fd6ff29ad56044b42c7878617321
Travis <travissheppard.1988@gmail.com>
2016-03-12 22:18:50 -0800

    simplify and show dna

--- src/entry.js
+++ src/entry.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ApplicationComponent from './application_component';
 import DNA from './components/dna';
 
 ReactDOM.render(<DNA />, document.getElementById('target'));
