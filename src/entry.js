8bf3602b33c56cd82262ca5ee81a4f770953da16
Travis <travissheppard.1988@gmail.com>
2016-03-19 16:55:56 -0700

    basic react

--- src/entry.js
+++ src/entry.js
@@ -1,1 +1,6 @@
-console.log('Compiled with webpack.  Edit me in src/entry.js');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Chart from './components/chart';
+
+ReactDOM.render(<Chart />, document.getElementById('target'));
