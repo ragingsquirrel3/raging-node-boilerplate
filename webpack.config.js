6239f040e4bb9e7724c27683dc85d533dce56ff5
Travis Sheppard <travissheppard.1988@gmail.com>
2016-04-02 15:39:41 -0700

    more babel

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,9 @@ module.exports = {
   },
   loaders: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel', // 'babel-loader' is also a legal name to reference
-        query: {
-          presets: ['react', 'es2015']
-        }
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader?presets[]=react,presets[]=es2015'],
       }
     ]
 };
