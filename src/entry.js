03cef995b199ea41bd2027aa5ba8cd81ea2d12f2
Travis <travissheppard.1988@gmail.com>
2016-04-10 11:06:27 -0700

    change typography

--- src/entry.js
+++ src/entry.js
@@ -6,8 +6,6 @@ import { createStore } from 'redux';
 import DNA from './components/dna';
 import dnaApp from './reducers';
 
-console.log('blah')
-
 // init redux store and render to target element in DOM
 let _store = createStore(dnaApp);
 render(
