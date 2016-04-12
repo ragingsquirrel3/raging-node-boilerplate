6e51ccf2feec79d6d654fbcd72d2f7afe3d3c0d7
Travis Sheppard <tshepp@stanford.edu>
2016-04-12 16:57:06 -0700

    add protein data

--- src/entry.js
+++ src/entry.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import d3 from 'd3';
 
 import Chart from './components/chart';
 
-ReactDOM.render(<Chart />, document.getElementById('target'));
+d3.json('protein.json', function (err, _data) {
+  _data = _data.map( (d,i) => {
+    d.id = i;
+    return d;
+  });
+	ReactDOM.render(<Chart data={_data}/>, document.getElementById('target'));
+});
+
