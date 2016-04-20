71e10277afe0acb14ee1dec97538478d493ef478
Travis <travissheppard.1988@gmail.com>
2016-04-20 09:44:38 -0700

    unzip again

--- src/components/dna.js
+++ src/components/dna.js
@@ -1,5 +1,6 @@
 import _ from 'underscore';
 import React from 'react';
+import d3 from 'd3';
 
 const DNA = React.createClass({
   getDefaultProps () {
@@ -22,8 +23,9 @@ const DNA = React.createClass({
   getInitialState() {
     return {
       currentStep: 0,
-      rnaPolPos: `-0.1 8 0`,
-      rnaPolCoord: 14
+      rnaPolY: 8,
+      rnaPolCoord: RNA_POL_START_COORD,
+      isSplit: true
     };
   },
 
@@ -35,14 +37,11 @@ const DNA = React.createClass({
   },
 
   render () {
-    console.log(this.state.currentStep)
-    // console.log('posL ', this.state.rnaPolPos, ' coord: ', this.state.rnaPolCoord)
     return (
       <div onClick={this._incrementStep}>
         <a-scene>
           {this._renderBillboard()}
-          {this._renderSection(`-2 0 0`)}
-          {this._renderSection(`-0.25 0 -5`, `0 37 110`)}
+          {this._renderSection(`0 0 0`)}
           {this._renderRNAPol()}
           <a-sky color='#272822' />
         </a-scene>
@@ -51,26 +50,32 @@ const DNA = React.createClass({
   },
 
   componentDidUpdate(prevProps, prevState) {
-    // if 0 -> 1 animate rnaPolPos
+    // if 0 -> 1 animate rnaPolY
     if (this.state.currentStep === 1 && prevState.currentStep === 0) {
-      let currentPos = this.state.rnaPolPos;
-      let currentY = parseInt(currentPos.split(' ')[1]);
-      const END_Y = 1.75;
-      this._interpolateNumber(currentY, END_Y, 1000, val => {
-        this.setState({ rnaPolPos: `-0.1 ${val} 0` });
+      const END_Y = 1.85;
+      this._interpolateNumber(this.state.rnaPolY, END_Y, 1000, val => {
+        this.setState({ rnaPolY: val});
       });
     }
     // if 1 -> 2 animate rnaPolCoord to go along DNA
+    if (this.state.currentStep === 2 && prevState.currentStep === 1) {
+      this.setState({ isSplit: true });
+      this._interpolateNumber(RNA_POL_START_COORD, RNA_POL_END_COORD, 10000, val => {
+        this.setState({ rnaPolCoord: val });
+      });
+    }
     // reset cleanup
     if (this.state.currentStep === 0 && prevState.currentStep > 0) {
-      this.setState({ rnaPolPos: `-0.1 8 0` });
-      if (this.interTimer) clearInterval(this.interTimer);
+      this.setState({
+        rnaPolY: 8,
+        rnaPolCoord: RNA_POL_START_COORD
+      });
     }
   },
 
   // cb(val)
   _interpolateNumber(start, end, duration, cb) {
-    const STEPS = 20;
+    const STEPS = duration / 1000 * 20;
     if (this.interTimer) clearInterval(this.interTimer);
     let current = start;
     let stepValue = (end - start) / STEPS;
@@ -87,17 +92,11 @@ const DNA = React.createClass({
   },
 
   _renderSection (_position, _rotation) {
-    let x = -(this.props.sequence.length / 2) * STEP_FACTOR_X;
     let y = 2;
-    let r = 0;
     const bpNodes = this.props.sequence.split('').map( (d, i) => {
-      let bpNode = this._renderBasePair(d);
-      x += STEP_FACTOR_X;
-      r += STEP_FACTOR_R;
-      // rotate strand
-      if (r > 360) {
-        r = 0;
-      }
+      let bpNode = this._renderBasePair(d, i);
+      let x = this._xScale(i);
+      let r = this._rScale(i);
       return <a-entity key={`bp${i}`} rotation={`${r} 0 0`} position={`${x} ${y} 0`}>{bpNode}</a-entity>;
     });
     _position = _position || '0 0 0';
@@ -111,11 +110,19 @@ const DNA = React.createClass({
 
   _renderRNAPol () {
     if (this.state.currentStep < 1) return null;
+    let x = this._xScale(this.state.rnaPolCoord);
+    let y = this.state.rnaPolY;
+    let _position = `${x} ${y} 0`;
+    let r = this._rScale(this.state.rnaPolCoord);
     return (
-      <a-entity position={this.state.rnaPolPos}>
-        <a-sphere radius={0.5} color={RNA_POL_COLOR} />
+      <a-entity position={_position} rotation={`${r} 0 0`}>
+        <a-sphere position={`0 ${-SIZE * 0.9} 0`} radius={SIZE / 3} color={RNA_POL_COLOR} />
       </a-entity>
     );
+  },
+
+  _renderMRNA () {
+
   },
 
   _renderBillboard () {
@@ -128,27 +135,37 @@ const DNA = React.createClass({
     return <a-image position={_position} width='9' height='9' id='billboard-img' src={_src} />;
   },
 
-  _getPlusOuterPos () {
-    let p = SIZE / 2;
+  _getSeparation (coord) {
+    const defaultVal = 0;
+    const max = 0.25;
+    const thresh = 3;
+    if (!this.state.isSplit) return defaultVal;
+    let distance = Math.abs(coord - this.state.rnaPolCoord);
+    if (distance > thresh) return defaultVal;
+    return (thresh - distance) * (max / thresh);
+  },
+
+  _getPlusOuterPos (coord) {
+    let p = this._getSeparation(coord) + SIZE / 2;
     return `0 -${p} 0`;
   },
 
-  _getMinusOuterPos () {
-    let p = SIZE / 2;
+  _getMinusOuterPos (coord) {
+    let p = this._getSeparation(coord) + SIZE / 2;
     return `0 ${p} 0`;
   },
 
-  _getPlusInnerPos () {
-    let p = SIZE / 4;
+  _getPlusInnerPos (coord) {
+    let p =this._getSeparation(coord) +  SIZE / 4;
     return `0 -${p} 0`;
   },
 
-  _getMinusInnerPos () {
-    let p = SIZE / 4;
+  _getMinusInnerPos (coord) {
+    let p = this._getSeparation(coord) + SIZE / 4;
     return `0 ${p} 0`;
   },
 
-  _renderBasePair (seqChar) {
+  _renderBasePair (seqChar, coord) {
     seqChar = seqChar.toLowerCase();
     // select primary and complementary nucleic acid colors from character
     let p, c;
@@ -172,13 +189,24 @@ const DNA = React.createClass({
     }
     return (
       <a-entity>
-        <a-sphere position={this._getPlusOuterPos()} radius={SIZE / 10} color={B_COLOR} />
-        <a-cylinder position={this._getPlusInnerPos()} radius={SIZE / 20} height={SIZE / 2} open-ended="false" color={p} />
-        <a-cylinder position={this._getMinusInnerPos()} radius={SIZE / 20} height={SIZE / 2} open-ended="false" color={c} />
-        <a-sphere position={this._getMinusOuterPos()} radius={SIZE / 10} color={B_COLOR} />
+        <a-sphere position={this._getPlusOuterPos(coord)} radius={SIZE / 10} color={B_COLOR} />
+        <a-cylinder position={this._getPlusInnerPos(coord)} radius={SIZE / 20} height={SIZE / 2} open-ended="false" color={p} />
+        <a-cylinder position={this._getMinusInnerPos(coord)} radius={SIZE / 20} height={SIZE / 2} open-ended="false" color={c} />
+        <a-sphere position={this._getMinusOuterPos(coord)} radius={SIZE / 10} color={B_COLOR} />
       </a-entity>
     );
-  }
+  },
+
+  _xScale (coord) {
+    const scale = d3.scale.linear()
+      .domain([0, 50])
+      .range([-6.125, 6.375]);
+    return scale(coord);
+  },
+
+  _rScale (coord) {
+    return (coord * STEP_FACTOR_R) % 360;
+  },
 });
 
 export default DNA;
@@ -192,6 +220,7 @@ const B_COLOR = '#3499FB';
 const RNA_POL_COLOR = '#E85379';
 
 const DEFAULT_BILLBOARD_POSITION = '0 1 -2';
+const RNA_POL_START_COORD = 0;
+const RNA_POL_END_COORD = 33;
 const SIZE = 0.75;
-const STEP_FACTOR_X = SIZE / 3;
 const STEP_FACTOR_R = 22;
