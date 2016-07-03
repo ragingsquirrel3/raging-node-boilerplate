1953ed8a03fea304410f15b45aa1bc8968512c46
Travis <travissheppard.1988@gmail.com>
2016-07-02 20:17:46 -0700

    draw some edges

--- src/index.js
+++ src/index.js
@@ -43,17 +43,45 @@ const ChordDiagram = React.createClass({
       .angle(function(d) { return d.x / 180 * Math.PI; });
     // prepare data
     var nodesData = cluster.nodes({ name: '', children: this.props.nodes });
+    var links = this._packageEdges(nodesData);
     // d3 DOM rendering
     var nodeTarget = d3.select(this.refs.nodeTarget);
     var node = nodeTarget.selectAll('.text-node')
       .data(nodesData.filter(function(n) { return !n.children; }))
-    .enter().append("text")
-      .attr("class", "text-node")
+    node
+      .enter().append("text")
+      .attr("class", "text-node");
+    node.exit().remove();
+    node.transition()
       .attr("dy", ".31em")
       .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + (d.y + 8) + ",0)" + (d.x < 180 ? "" : "rotate(180)"); })
       .style("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-      .text(function(d) { return d.name; })
+      .text(function(d) { return d.name; });
+    var link = nodeTarget.selectAll('.link-node')
+      .data(bundle(links));
+    link.enter().append("path")
+      .attr("class", "link-node")
+      .attr("fill", "none")
+      .attr("stroke", "steelblue");
+    link.exit().remove();
+    link.transition()
+      .attr("d", line);
+  },
+
+  _packageEdges (nodes) {
+    return this.props.edges.map( (d, i) => {
+      return { source: this._findNodeById(d.source), target: this._findNodeById(d.target) };
+    });
+  },
+
+  _findNodeById (id) {
+    for (var i = this.props.nodes.length - 1; i >= 0; i--) {
+      let thisNode = this.props.nodes[i];
+      if (thisNode.id === id) return thisNode;
+    }
+    return null;
   }
+
 });
 
 export default ChordDiagram;
